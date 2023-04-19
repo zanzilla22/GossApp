b2e254d21574d3d58ae84195bcaeaa652b6d2544
zanzilla22 <69683242+zanzilla22@users.noreply.github.com>
2023-04-19 09:31:48 -0400

    modular profile function

--- GossApp/App.js
+++ GossApp/App.js
@@ -2,25 +2,21 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View , Image, TouchableOpacity} from 'react-native';
 
-export default function App() {
+const ProfileCard = ({ name, school, picture}) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!  What's your opinion on minorities? </Text>
-      <StatusBar style="auto" />
-      <View style={{ flex: 1, backgroundColor: '#9de2ff', justifyContent: 'center', alignItems: 'center' }}>
-      <View style={{backgroundColor: '#fff', borderRadius: 10, padding: 20 }}>
+  <View style={{backgroundColor: '#fff', borderRadius: 30, padding: 10 }}>
         <View style={{ borderRadius: 15 }}>
           <View style={{ padding: 20 }}>
             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
               <Image
-                source={{ uri: 'https://i.imgur.com/lyuRGpc.jpeg' }}
+                source={{ uri: picture }}
                 style={{ width: 180, height: 180, borderRadius: 10 }}
               />
               <View style={{ marginLeft: 20 }}>
-                <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 5 }}>Zane Beeai</Text>
-                <Text style={{ fontSize: 18, color: '#2b2a2a', marginBottom: 5 }}>St. Thomas Aquinas</Text>
+                <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 5 }}>{name}</Text>
+                <Text style={{ fontSize: 18, color: '#2b2a2a', marginBottom: 5 }}>{school}</Text>
 
-                <View style={{ flexDirection: 'row', marginTop: 5 , marginBotton:20}}>
+                <View style={{ flexDirection: 'row', marginTop: 5 , marginBottom:20}}>
                   <TouchableOpacity style={{ flex: 1, marginRight: 10, borderWidth: 1, borderColor: 'blue', borderRadius: 10 }}>
                     <Text style={{ fontSize: 18, color: 'blue', textAlign: 'center', paddingVertical: 10 }}>Remove</Text>
                   </TouchableOpacity>
@@ -50,7 +46,26 @@ export default function App() {
           </View>
         </View>
       </View>
-    </View>
+  )
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!  What's your opinion on minorities? </Text>
+      <StatusBar style="auto" />
+      <View style={{ flex: 1, backgroundColor: '#9de2ff', justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{ flex: 1 }}>
+          <ProfileCard name="Zane Beeai" school="St. Thomas Aquinas" picture = "https://i.imgur.com/lyuRGpc.jpeg"/>
+        </View>
+        <View style={{ flex: 1 }}>
+          <ProfileCard name="Zane Beeai" school="St. Thomas Aquinas" picture = "https://i.imgur.com/lyuRGpc.jpeg"/>
+        </View>
+        <View style={{ flex: 1 }}>
+          <ProfileCard name="Zane Beeai" school="St. Thomas Aquinas" picture = "https://i.imgur.com/lyuRGpc.jpeg"/>
+        </View>
+        
+      </View>
     </View>
   );
 }
