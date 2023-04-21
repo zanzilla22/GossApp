b470601924b9b648773b06ba2dd4ac3602d7fdcc
zanzilla22 <zanezilla22@gmail.com>
2023-04-20 23:07:19 -0400

    like button !!
    basically fucking gave up on that old database setup lmao

--- GossApp/App.js
+++ GossApp/App.js
@@ -1,43 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View , Image, TouchableOpacity, ScrollView, Vibration} from 'react-native';
 
 
-import React, { useState } from 'react';
-import { 
-  TouchableOpacity, 
-  StyleSheet, 
-  // Vibration 
-} from 'react-native';
+const LikeButton = ({ onPress }) => {
+  const [liked, setLiked] = useState(false);
 
-// const LikeButton = ({ onPress }) => {
-//   const [liked, setLiked] = useState(false);
+  const handlePress = () => {
+    setLiked(!liked);
+    onPress();
+    Vibration.vibrate(100); // Vibrate for 100 milliseconds
+  };
 
-//   const handlePress = () => {
-//     setLiked(!liked);
-//     onPress();
-//     Vibration.vibrate(100); // Vibrate for 100 milliseconds
-//   };
+  return (
+    <TouchableOpacity onPress={handlePress} style={styles.likeButton}>
+      {liked ? (
+        <Image
+        source={{ uri: 'https://i.imgur.com/aISngLq.png' }}
+        style={{ width: 35, height: 35}}
+      />
+      ) : (
+        <Image
+                source={{ uri: 'https://i.imgur.com/3qCp9DB.png' }}
+                style={{ width: 35, height: 35 }}
+              />
+      )}
+    </TouchableOpacity>
+  );
+};
 
-//   return (
-//     <TouchableOpacity onPress={handlePress} style={styles.container}>
-//       {liked ? (
-//         <Image
-//         source={{ uri: 'https://i.imgur.com/MgbEYie.png' }}
-//         style={{ width: 160, height: 160, borderRadius: 10 }}
-//       />
-//       ) : (
-//         <Image
-//                 source={{ uri: 'https://i.imgur.com/rM2Jvyg.png' }}
-//                 style={{ width: 160, height: 160, borderRadius: 10 }}
-//               />
-//       )}
-//     </TouchableOpacity>
-//   );
-// };
+const addLike = () => {
+  console.log("Like added!")
+}
 
-
-const ProfileCard = ({ name, school, year, picture}) => {
+const ProfileCard = ({ name, school, year, picture, onMessagePress}) => {
   return (
   <View style={{backgroundColor: '#fff', borderRadius: 30, padding: 5 }}>
         <View style={{ borderRadius: 15 }}>
@@ -52,12 +48,11 @@ const ProfileCard = ({ name, school, year, picture}) => {
                 <Text style={{ fontSize: 18, color: '#2b2a2a', marginBottom: 5, width: 160}}>{school} - {year}</Text>
 
                 <View style={{ flexDirection: 'row', marginTop: 5 , marginBottom:20, width: 160,}}>
+                
                   <TouchableOpacity style={{ flex: 1, marginRight: 10, borderWidth: 1, borderColor: 'blue', borderRadius: 10 }}>
                     <Text style={{ fontSize: 18, color: 'blue', textAlign: 'center', paddingVertical: 10 }}>Remove</Text>
                   </TouchableOpacity>
-                  <TouchableOpacity style={{ flex: 1, marginLeft: 10, backgroundColor: 'blue', borderRadius: 10 }}>
-                    <Text style={{ fontSize: 18, color: '#fff', textAlign: 'center', paddingVertical: 10 }}>Like</Text>
-                  </TouchableOpacity>
+                  <LikeButton onPress = {addLike}/>
                 </View>
 
 
@@ -71,7 +66,8 @@ const ProfileCard = ({ name, school, year, picture}) => {
                 
               </View>
             </View>
-            <TouchableOpacity style={{marginTop:10, flex: 1, marginLeft: 10, backgroundColor: '#fc90c3', borderRadius: 10 }}>
+            <TouchableOpacity onPress={onMessagePress}
+            style={{marginTop:10, flex: 1, marginLeft: 10, backgroundColor: '#fc90c3', borderRadius: 10 }}>
                     <Text style={{ fontSize: 18, color: '#fff', textAlign: 'center', padding: 10 }}>Chat as Secret Admirer</Text>
                 </TouchableOpacity>
           </View>
@@ -81,6 +77,9 @@ const ProfileCard = ({ name, school, year, picture}) => {
 }
 
 export default function App() {
+  const [currentView, setCurrentView] = useState('profiles');
+  const [selectedProfile, setSelectedProfile] = useState(null);
+
   const profiles = [
     { name: "Zane Beea", school: "St. Thomas Aquinas", year: "07", picture: "https://i.imgur.com/lyuRGpc.jpeg" },
     { name: "Aditya Raman", school: "University of Toronto Schools", year: "07", picture: "https://i.imgur.com/nvXkH5B.png" },
@@ -88,24 +87,51 @@ export default function App() {
     { name: "John Doe", school: "MIT", year: "03", picture: "https://i.imgur.com/X6IJX5g.jpeg" }
   ];
 
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! What's your opinion on minorities?</Text>
-      <StatusBar style="auto" />
-      <View style={{ flex: 1, backgroundColor: '#d9c1cc', justifyContent: 'center', alignItems: 'center' }}>
-        <View style={{ flex: 1, overflow: 'hidden' }}>
-          <ScrollView contentContainerStyle={{ paddingHorizontal: 20 }}>
-            {profiles.map((profile, index) => (
-              <View key={index} style={{ marginBottom: 20 }}>
-                <ProfileCard name={profile.name} school={profile.school} year={profile.year} picture={profile.picture} />
-              </View>
-            ))} 
-          </ScrollView>
+  const handleProfileSelection = (profile) => {
+    setSelectedProfile(profile);
+    setCurrentView('chat');
+  };
+
+  const handleBackToProfiles = () => {
+    setCurrentView('profiles');
+  };
+
+  if (currentView === 'profiles') {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app! What's your opinion on minorities?</Text>
+        <StatusBar style="auto" />
+        <View style={{ flex: 1, backgroundColor: '#d9c1cc', justifyContent: 'center', alignItems: 'center' }}>
+          <View style={{ flex: 1, overflow: 'hidden' }}>
+            <ScrollView contentContainerStyle={{ paddingHorizontal: 20 }}>
+              {profiles.map((profile, index) => (
+                <View key={index} style={{ marginBottom: 20 }}>
+                  <ProfileCard
+                    name={profile.name}
+                    school={profile.school}
+                    year={profile.year}
+                    picture={profile.picture}
+                    onMessagePress={() => handleProfileSelection(profile)}
+                  />
+                </View>
+              ))}
+            </ScrollView>
+          </View>
         </View>
       </View>
-    </View>
-  );
+    );
+  } else if (currentView === 'chat') {
+    return (
+      <View style={styles.container}>
+        <Text>Chat with {selectedProfile.name}</Text>
+        <TouchableOpacity onPress={handleBackToProfiles} style={{ marginTop: 20 }}>
+          <Text>Back to profiles</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
+
 
 
 
@@ -113,6 +139,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  likeButton: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
