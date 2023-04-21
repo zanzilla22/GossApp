dab815cf9e60cadc42d7b9d4febba408954e270e
Aditya Raman <114894627+theadityaraman@users.noreply.github.com>
2023-04-21 09:38:07 -0400

    firebase slight fix
    imported wrong version we have skill issues

--- GossApp/firebase.js
+++ GossApp/firebase.js
@@ -1,11 +1,6 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from "firebase"
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-import { initializeApp } from 'firebase/app'
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -20,16 +15,13 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-
-
 let app;
-if(firebase.apps.length ===0){
-    app = initializeApp(firebaseConfig);
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
 } else {
-    app = firebase.app()
+  app = getApp();
 }
-// const analytics = getAnalytics(app);
 
-const auth = firebase.auth()
+const auth = getAuth(app);
 
-export {auth};
+export { auth };
