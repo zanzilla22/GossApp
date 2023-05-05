49e8ea6cdf7a644a1e7065e9ae437835e16d0b6e
Aditya Raman <114894627+theadityaraman@users.noreply.github.com>
2023-05-05 01:00:38 -0400

    Update styles.js
    this probably worked

--- GossApp/Screens/styles.js
+++ GossApp/Screens/styles.js
@@ -1,23 +1,22 @@
 module.exports = {
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f5f5f5',
     padding: 16,
     justifyContent: 'center',
     alignItems: 'center',
+    paddingTop: 50,
   },
   container2: {
     flex: 1,
     backgroundColor: '#fff',
     padding: 16,
     justifyContent: 'center',
-    // alignItems: 'center',
   },
   container3: {
     backgroundColor: '#f0f0f0',
-    padding:20,
-    borderRadius:15,
-    
+    padding: 20,
+    borderRadius: 15,
   },
   container4: {
     flex: 1,
@@ -27,45 +26,60 @@ module.exports = {
     flex: 1,
     padding: 16,
   },
+  messageContainer: {
+    flex: 1,
+    padding: 10,
+  },
   userMessage: {
     alignSelf: 'flex-end',
-    backgroundColor: '#0099FF',
-    borderRadius: 8,
-    padding: 8,
-    marginBottom: 8,
-    maxWidth: '70%',
+    backgroundColor: '#0084ff',
+    borderRadius: 20,
+    padding: 10,
+    marginBottom: 10,
+    maxWidth: '80%',
+  },
+  userMessageText: {
+    color: '#ffffff',
   },
   chatGPTMessage: {
     alignSelf: 'flex-start',
     backgroundColor: '#E5E5EA',
-    borderRadius: 8,
-    padding: 8,
-    marginBottom: 8,
-    maxWidth: '70%',
+    borderRadius: 20,
+    padding: 10,
+    marginBottom: 10,
+    maxWidth: '80%',
+  },
+  botMessageText: {
+    color: '#333333',
   },
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     padding: 16,
+    paddingHorizontal: 10,
     borderTopWidth: 1,
     borderTopColor: '#E5E5EA',
+    backgroundColor: '#ffffff',
   },
   input: {
     flex: 1,
     borderWidth: 1,
     borderColor: '#E5E5EA',
-    borderRadius: 8,
+    borderRadius: 50,
     padding: 8,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
     marginRight: 8,
+    backgroundColor: '#f5f5f5',
   },
   sendButton: {
-    backgroundColor: '#0099FF',
-    borderRadius: 8,
-    padding: 8,
+    marginLeft: 10,
+    padding: 10,
+    backgroundColor: '#0084ff',
+    borderRadius: 50,
   },
   sendButtonText: {
     color: '#FFFFFF',
-    fontWeight: 'bold',
   },
   title: {
     fontSize: 24,
@@ -99,7 +113,7 @@ module.exports = {
     textAlign: 'center',
   },
   inputContainer2: {
-    width: '80%'
+    width: '80%',
   },
   input2: {
     backgroundColor: 'white',
@@ -107,7 +121,7 @@ module.exports = {
     paddingVertical: 10,
     borderRadius: 10,
     marginTop: 5,
-    marginBottom:10,
+    marginBottom: 10,
   },
   buttonContainer: {
     width: '60%',
@@ -120,70 +134,3 @@ module.exports = {
     width: '100%',
     padding: 15,
     borderRadius: 10,
-    alignItems: 'center',
-  },
-  buttonOutline: {
-    backgroundColor: 'white',
-    marginTop: 5,
-    borderColor: '#0782F9',
-    borderWidth: 2,
-  },
-  buttonText: {
-    color: 'white',
-    fontWeight: '700',
-    fontSize: 16,
-  },
-  buttonOutlineText: {
-    color: '#0782F9',
-    fontWeight: '700',
-    fontSize: 16,
-  },
-  pickerContainer: {
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 5,
-    paddingHorizontal: 10,
-  },
-  picker: {
-    height: 50,
-  },
-  selectedValue: {
-    marginTop: 10,
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  dropdown: {
-    height: 50,
-    borderColor: 'gray',
-    borderWidth: 0.5,
-    borderRadius: 8,
-    paddingHorizontal: 8,
-    marginBottom:10,
-  },
-  icon: {
-    marginRight: 5,
-  },
-  label: {
-    position: 'absolute',
-    backgroundColor: 'white',
-    left: 22,
-    top: 8,
-    zIndex: 999,
-    paddingHorizontal: 8,
-    fontSize: 14,
-  },
-  placeholderStyle: {
-    fontSize: 16,
-  },
-  selectedTextStyle: {
-    fontSize: 16,
-  },
-  iconStyle: {
-    width: 20,
-    height: 20,
-  },
-  inputSearchStyle: {
-    height: 40,
-    fontSize: 16,
-  },
-};
